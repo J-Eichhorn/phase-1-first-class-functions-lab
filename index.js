5f5e0f7b92c11f6f14c675772b8baaee917f54e5
Joshua Eichhorn <JoshuaEichhorn@ymail.com>
2023-03-20 14:28:19 -0400

    passes tests

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(array){
+    let newArray = [];
+    return newArray = array.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(array){
+    let newArray = [];
+    return newArray = array.slice(-2, array.length);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+createFareMultiplier = int => fare => fare * int;
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers, selectingDrivers){
+    return selectingDrivers(arrayOfDrivers);
+}
